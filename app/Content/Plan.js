8aac3cdaeac6830cac841a8416b54c4d6c4b7db1
Simon <greenspanx@hotmail.com>
2019-09-14 22:17:23 +0800

    public content plans

---
+++ app/Content/Plan.js
@@ -0,0 +1,17 @@
+const Entry = [
+
+    {
+        lesson: 'Manual Lessons',        
+        price: '$55/hr',        
+    },
+    {
+        lesson: 'Automatic Lessons',
+        price: '$50/hr',        
+    },
+    {
+        lesson: 'Students Lessons',        
+        price: '$45/hr',        
+    },
+]
+
+export default Entry;
