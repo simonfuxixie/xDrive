738ff6a7ce97434d6477da4856584d0e2e315cd5
Simon <greenspanx@hotmail.com>
2019-09-14 22:11:54 +0800

    public content highlights

---
+++ app/Content/Highlights.js
@@ -0,0 +1,9 @@
+const Entry = [
+    {
+        title: 'THE BEST',
+        desc: 'Driving School in Perth',
+        image: require('@Asset/images/slider-1.jpg')
+    }    
+]
+
+export default Entry;
