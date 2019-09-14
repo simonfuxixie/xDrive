0ad90ccab6818fda224418273cd6822919ed6c13
Simon <greenspanx@hotmail.com>
2019-09-14 22:19:20 +0800

    public content style.js

---
+++ app/Content/Style.js
@@ -0,0 +1,226 @@
+export default {
+
+  logo: {
+  },
+
+  /** Highlight **/
+  sliderList: {
+    paddingLeft: 10,
+    marginTop: 15
+  },
+  sliderBig: {
+    width: 320,
+    height: 220,
+    borderRadius: 10,
+    elevation: 10,
+    shadowOffset: {
+      width: 15,
+      height: 15
+    },
+    shadowColor: '#000',
+    shadowOpacity: 0.5,
+    shadowRadius: 0,
+    margin: 10,
+    marginBottom: 20
+  },
+  sliderImg: {
+    marginBottom: 10,
+    width: '100%',
+    height: 220,
+    borderRadius: 10
+  },
+  sliderOverlay: {
+    width: 320,
+    height: 220,
+    position: 'absolute',
+    bottom: 10
+  },
+  sliderContent: {
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%'
+  },
+  sliderTitle: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 16,
+    color: '#EFBC0E'
+  },
+  sliderDesc: {
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 11,
+    color: '#FFF'
+  },
+  sliderBtn: {
+    padding: 5,
+    backgroundColor: '#EFBC0E',
+    borderRadius: 3
+  },
+  sliderBtnText: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 10,
+    color: '#FFF'
+  },
+
+  /** Lesson **/
+  lesson: {
+    paddingVertical: 15,
+    paddingHorizontal: 15
+  },
+  lessonHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 15,
+    fontFamily: 'Montserrat-SemiBold',
+    color: '#EFBC0E',
+    fontSize: 14
+  },
+  lessonItem: {
+    flexDirection: 'row',
+    padding: 15,
+    backgroundColor: '#282828',
+    marginBottom: 1,
+    alignItems: 'center',
+    borderRadius: 10
+  },
+  lessonItemContent: {
+    flexWrap: 'wrap',
+    flex: 1
+  },
+  lessonItemTitle: {
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 12,
+    color: 'rgba(255,255,255,0.8)',
+    marginBottom: 5
+  },
+  lessonItemDesc: {
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 11,
+    color: 'rgba(255,255,255,0.4)',
+    lineHeight: 16
+  },
+  lessonItemBtnIcon: {
+    color: '#EFBC0E',
+    fontSize: 18
+  },
+  lessonItemImg: {
+    marginLeft: 10,
+    marginRight: 20
+  },
+
+  /** Plan **/
+  plan: {
+    paddingBottom: 15,
+    paddingHorizontal: 15
+  },
+  planHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 15,
+    fontFamily: 'Montserrat-SemiBold',
+    color: '#EFBC0E',
+    fontSize: 14
+  },
+  planItem: {
+    flexDirection: 'row',
+    width: 150,
+    padding: 15,
+    backgroundColor: '#282828',
+    marginRight: 10,
+    justifyContent: 'center',
+    borderRadius: 10
+  },
+  planItemGet: {
+    fontFamily: 'Montserrat-SemiBold',
+    color: 'rgba(255,255,255,0.8)',
+    fontSize: 14,
+    alignSelf: 'center',
+    marginBottom: 10
+  },
+  planItemLesson: {
+    fontFamily: 'Montserrat-SemiBold',
+    color: '#EFBC0E',
+    fontSize: 12,
+    textAlign: 'center',
+    marginBottom: 15
+  },
+  planItemStart: {
+    fontFamily: 'Montserrat-Regular',
+    color: 'rgba(255,255,255,0.5)',
+    fontSize: 12,
+    textAlign: 'center'
+  },
+  planItemPriceFrom: {
+    fontFamily: 'Montserrat-SemiBold',
+    color: '#EFBC0E',
+    fontSize: 18,
+    textAlign: 'center',
+    marginVertical: 10
+  },
+  planItemDesc: {
+    fontFamily: 'Montserrat-Regular',
+    color: 'rgba(255,255,255,0.5)',
+    fontSize: 12,
+    textAlign: 'center'
+  },
+  planItemPriceTo: {
+    fontFamily: 'Montserrat-SemiBold',
+    color: '#EFBC0E',
+    fontSize: 18,
+    alignSelf: 'center',
+    marginVertical: 10
+  },
+
+  /** Attributes **/
+  attributes: {
+    paddingVertical: 15,
+    paddingLeft: 15
+  },
+  attributesHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 15,
+    fontFamily: 'Montserrat-SemiBold',
+    color: '#EFBC0E',
+    fontSize: 14,
+    textAlign: 'right',
+    paddingRight: 15
+  },
+  attributesItem: {
+    flexDirection: 'row',
+    width: 80,
+    padding: 15,
+    marginLeft: 15,
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginTop: 30
+  },
+  attributesImg: {
+    flex: 1,
+    minHeight: 250,
+    alignItems: 'center',
+    backgroundColor: '#222'
+  },
+  attributesMain: {
+    marginLeft: 20
+  },
+  attributesImgBg: {
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    marginBottom: 10
+  },
+  attributesTitle: {
+    fontFamily: 'Montserrat-SemiBold',
+    color: 'rgba(0,0,0,0.8)',
+    fontSize: 12,
+    textAlign: 'center'
+  }
+
+}
