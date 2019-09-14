0aa978622e244234d006fd1436c49505a3611104
Simon <greenspanx@hotmail.com>
2019-09-14 22:16:06 +0800

    public content lessons

---
+++ app/Content/Lesson.js
@@ -0,0 +1,24 @@
+const Entry = [
+    {
+        name: 'Beginner',
+        desc: 'To Help you Kick-start your course.',
+        image: require('@Asset/images/lesson-beginner.png')
+    },
+    {
+        name: 'Lapsed Learner',
+        desc: 'If your first lessons or practical test didn\'t go well. We\'ll help you build confidence.',
+        image: require('@Asset/images/lesson-lapsed.png')
+    },
+    {
+        name: 'Refresher Lessons',
+        desc: 'If you\'ve got a valid driving license but havent used your skills for a while.',
+        image: require('@Asset/images/lesson-refresher.png')
+    },
+    {
+        name: 'Pass Plus Lessons',
+        desc: 'A Pass Plus Lessons is a seemless way to fine tune your driving skils and can save you money on your car insurance. ',
+        image: require('@Asset/images/lesson-pass.png')
+    },
+]
+
+export default Entry;
