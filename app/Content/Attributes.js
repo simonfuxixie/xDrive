d42c2ef1ff2dfb11096dd7ee0647bb5b1151d5b9
Simon <greenspanx@hotmail.com>
2019-09-14 22:09:26 +0800

    public content attributes

---
+++ app/Content/Attributes.js
@@ -0,0 +1,25 @@
+const Entry = [
+
+    {
+        title: 'Mock Test',
+        image: require('@Asset/images/attributes-mocktest.png')
+    },
+    {
+        title: 'Driving Lessons',
+        image: require('@Asset/images/attributes-drivingl-lessons.png')
+    },
+    {
+        title: 'Coverage Area',
+        image: require('@Asset/images/attributes-coverage-area.png')
+    },
+    {
+        title: 'Experience Instructors',
+        image: require('@Asset/images/attributes-experience.png')
+    },
+    {
+        title: 'Door Pickup',
+        image: require('@Asset/images/attributes-door-pickup.png')
+    },
+]
+
+export default Entry;
