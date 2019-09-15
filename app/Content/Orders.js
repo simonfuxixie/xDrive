566c11977dd4750576066b7a32631cae2ff0a09e
Simon <greenspanx@hotmail.com>
2019-09-15 21:16:52 +0800

    mock orders

---
+++ app/Content/Orders.js
@@ -0,0 +1,47 @@
+const Entry = [
+
+    {
+        orderid: '1',
+        name: 'Manual Lessons',
+        orderno: 'DGDGD1H09782K5141672H',
+        date: '20 Nov',
+        price: '$50',
+    },
+    {
+        orderid: '2',
+        name: 'Students Lessons',
+        orderno: 'KO808709782K5141672H',
+        date: '19 Nov',
+        price: '$45',
+    },
+    {
+        orderid: '3',
+        name: 'Advance Lessons',
+        orderno: '35353DG9782K5141672H',
+        date: '17 Nov',
+        price: '$50',
+    },
+    {
+        orderid: '4',
+        name: 'Manual Lessons',
+        orderno: 'DGDGD1H09782K5141672H',
+        date: '10 Nov',
+        price: '$50',
+    },
+    {
+        orderid: '5',
+        name: 'Students Lessons',
+        orderno: 'KO808709782K5141672H',
+        date: '5 Nov',
+        price: '$45',
+    },
+    {
+        orderid: '6',
+        name: 'Advance Lessons',
+        orderno: '35353DG9782K5141672H',
+        date: '1 Nov',
+        price: '$50',
+    },
+]
+
+export default Entry;
