bca812600c5d4f44291eabdee1c639705b10c668
Simon <greenspanx@hotmail.com>
2019-09-15 20:32:31 +0800

    time

---
+++ app/Content/Time.js
@@ -0,0 +1,49 @@
+const Entry = [
+
+    {
+        time: '08:00',
+        session: 'AM',
+    },    
+    {
+        time: '09:00',
+        session: 'AM',
+    },    
+    {
+        time: '10:00',
+        session: 'AM',
+    },    
+    {
+        time: '11:00',
+        session: 'AM',
+    },    
+    {
+        time: '12:00',
+        session: 'PM',
+    },    
+    {
+        time: '01:00',
+        session: 'PM',
+    },    
+    {
+        time: '02:00',
+        session: 'PM',
+    },    
+    {
+        time: '03:00',
+        session: 'PM',
+    },    
+    {
+        time: '04:00',
+        session: 'PM',
+    },    
+    {
+        time: '05:00',
+        session: 'PM',
+    },    
+    {
+        time: '06:00',
+        session: 'PM',
+    },
+]
+
+export default Entry;
